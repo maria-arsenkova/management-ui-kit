9ac172848aee040cc664600f8762d1e72a27c091
Maria <1709rt@gmail.ru>
2021-02-18 15:41:09 +0300

    create component stats

--- my-app/src/Components/Sidebar.tsx
+++ my-app/src/Components/Sidebar.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 import '../style.css';
 import Brand from './Brand';
 import UserInfo from './UserInfo';
+import Stats from './Stats';
 import userpic from '../img/userpic.png';
 
 const user = {
   avatar: userpic,
   initials: 'Emilee Simchenko',
   position: 'Product Owner'
+}
+
+const tasks = {
+  completed: {
+    name: 'Completed Tasks',
+    counter: '372'
+  },
+  open: {
+    name: 'Open Tasks',
+    counter: '11'
+  },
 }
     function Sidebar () {
         return (
@@ -18,6 +30,10 @@ const user = {
             initials={user.initials}
             position={user.position}
            />
+          <Stats 
+            completed={tasks.completed}
+            open={tasks.open}
+          />
         </div>
         )
     }
---
+++ my-app/src/Components/Stats.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../style.css';
+
+
+function Stats(props: any){
+    return( 
+        <div className="stats">
+            <div>
+                <div className="stats-counter">
+                    {props.completed.counter}
+                </div>
+                <div className="stats-name">
+                    {props.completed.name}
+                </div>
+            </div>
+            <div>
+                <div className="stats-counter">
+                    {props.open.counter}
+                </div>
+                <div className="stats-name">
+                    {props.open.name}
+                </div>
+            </div>
+        </div>
+    );  
+  }
+
+export default Stats;
