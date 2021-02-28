9e458600edd4e01fec4feb75b2a6e30cd06d71ad
Maria <1709rt@gmail.ru>
2021-02-28 18:36:16 +0300

    created Component approach

--- my-app/src/Components/Brand/index.tsx
+++ my-app/src/Components/Brand/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import '../style.css';
-import logo from '../img/logo.png';
-import search from '../img/search.png';
+import './index.css';
+import logo from './logo.png';
+import search from './search.png';
 
 function Brand(){
     return( 
