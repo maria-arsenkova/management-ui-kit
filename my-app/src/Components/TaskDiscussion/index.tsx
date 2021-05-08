ee7498a05b6651d7ae34227571fe1622c13a37e1
Maria <1709rt@gmail.ru>
2021-05-08 15:50:39 +0300

    edit style css components

--- my-app/src/Components/TaskDiscussion/index.tsx
+++ my-app/src/Components/TaskDiscussion/index.tsx
@@ -69,7 +69,7 @@ function TaskDiscussion({
         </button>
       </div>
       {content && (
-        <div className="test">
+        <div>
           {content.map((item) => {
             return (
               <Comment
