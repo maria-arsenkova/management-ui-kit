9c959f8235790c8571e06467c12ad216f88b3dca
Maria <1709rt@gmail.ru>
2021-06-10 19:29:01 +0300

    edited table

--- src/Components/Files/index.tsx
+++ src/Components/Files/index.tsx
@@ -1,44 +1,80 @@
 import React from 'react';
 import './style.css';
 import { TaskType } from '../Task/types';
+import searchIcon from './img/searchIcon.svg';
+import arrowIcon from './img/arrowIcon.svg';
+import downloadIcon from './img/downloadIcon.svg';
 
 interface FilesProps {
   tasks: TaskType[];
 }
 
 function Files({ tasks }: FilesProps) {
-  //   console.log(tasks.filter((el) => el.files));
-
-  const allFiles = tasks.map(function (item) {
-    return item.files;
-  });
-  console.log(allFiles);
-
   return (
     <div className='files'>
       <table className='files__table'>
-        <tr>
-          <th>Image</th>
-          <th>Name</th>
-          <th>Size</th>
-          <th>Uploaded By</th>
-          <th>Tag</th>
-          <th>Tag</th>
-          <th>Date</th>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
-          <td></td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>
+              <img src={searchIcon} alt='searchIcon' className='searchIcon' />
+              Name
+            </th>
+            <th>Size</th>
+            <th>Uploaded By</th>
+            <th>Date</th>
+            <th>Tag</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        {tasks.map((item) => {
+          return (
+            //добавить ключь
+            //tbody дублируется с каждое tr, return требует иметь одного обобщаюшего элем
+            <tbody>
+              {item?.files &&
+                item.files.map((item) => {
+                  return (
+                    <tr key={`${item.uploadedBy}_${item.name}`}>
+                      <td>
+                        <img
+                          className='table__img'
+                          src={item.preview}
+                          alt={item.name}
+                          key={`${item.preview}_${item.name}`}
+                        />
+                      </td>
+                      <td>{item.name}</td>
+                      <td>
+                        {item.size} {item.sizeSign}
+                      </td>
+                      <td>{item.uploadedBy}</td>
+                      <td>{item.date}</td>
+                      <td></td>
+                      <td>
+                        <button className='table__button'>
+                          Actions{' '}
+                          <img
+                            src={arrowIcon}
+                            alt='arrowIcon'
+                            key={`${arrowIcon}_${item.name}`}
+                          />
+                        </button>
+                      </td>
+                      <td>
+                        <img
+                          src={downloadIcon}
+                          alt='downloadIcon'
+                          key={`${downloadIcon}_${item.name}`}
+                        />
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          );
+        })}
       </table>
     </div>
   );
