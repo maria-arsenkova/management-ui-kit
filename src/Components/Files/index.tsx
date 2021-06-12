e5bb63366a7eec3313a7d185fbded031c7dd82d2
ilyapasyuk <pasyuk.com@gmail.com>
2021-06-12 13:39:17 +0300

    update `Files` component

--- src/Components/Files/index.tsx
+++ src/Components/Files/index.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { HashRouter, Switch, Route, Redirect, Link } from 'react-router-dom';
-import './style.css';
-import { TaskType } from '../Task/types';
-import searchIcon from './img/searchIcon.svg';
-import arrowIcon from './img/arrowIcon.svg';
-import downloadIcon from './img/downloadIcon.svg';
+import React from "react";
+import "./style.css";
+import { TaskFilesType } from "../TaskFiles/types";
+import searchIcon from "./img/searchIcon.svg";
+import arrowIcon from "./img/arrowIcon.svg";
+import downloadIcon from "./img/downloadIcon.svg";
 
 interface FilesProps {
-  tasks: TaskType[];
+  files: TaskFilesType[];
 }
 
-function Files({ tasks }: FilesProps) {
+function Files({ files }: FilesProps) {
   return (
-    <div className='files'>
-      <table className='files__table'>
+    <div className="files">
+      <table className="files__table">
         <thead>
           <tr>
             <th>Image</th>
             <th>
-              <img src={searchIcon} alt='searchIcon' className='searchIcon' />
+              <img src={searchIcon} alt="searchIcon" className="searchIcon" />
               Name
             </th>
             <th>Size</th>
@@ -29,55 +28,48 @@ function Files({ tasks }: FilesProps) {
             <th></th>
           </tr>
         </thead>
-        {tasks.map((item) => {
-          return (
-            //добавить ключь
-            //tbody дублируется с каждое tr, return требует иметь одного обобщающего элем
-            <tbody>
-              {item?.files &&
-                item.files.map((item) => {
-                  return (
-                    <tr key={`${item.uploadedBy}_${item.name}`}>
-                      <td>
-                        <img
-                          className='table__img'
-                          src={item.preview}
-                          alt={item.name}
-                          key={`${item.preview}_${item.name}`}
-                        />
-                      </td>
-                      <td>{item.name}</td>
-                      <td>
-                        {item.size} {item.sizeSign}
-                      </td>
-                      <td>{item.uploadedBy}</td>
-                      <td>{item.date}</td>
-                      <td></td>
-                      <td>
-                        <button className='table__button'>
-                          Actions{' '}
-                          <img
-                            src={arrowIcon}
-                            alt='arrowIcon'
-                            key={`${arrowIcon}_${item.name}`}
-                          />
-                        </button>
-                      </td>
-                      <td>
-                        <a target='_blank' href={item.preview} download>
-                          <img
-                            src={downloadIcon}
-                            alt='downloadIcon'
-                            key={`${downloadIcon}_${item.name}`}
-                          />
-                        </a>
-                      </td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          );
-        })}
+        <tbody>
+          {files.map((item) => {
+            return (
+              <tr key={`${item.uploadedBy}_${item.name}`}>
+                <td>
+                  <img
+                    className="table__img"
+                    src={item.preview}
+                    alt={item.name}
+                    key={`${item.preview}_${item.name}`}
+                  />
+                </td>
+                <td>{item.name}</td>
+                <td>
+                  {item.size} {item.sizeSign}
+                </td>
+                <td>{item.uploadedBy}</td>
+                <td>{item.date}</td>
+                <td></td>
+                <td>
+                  <button className="table__button">
+                    Actions{" "}
+                    <img
+                      src={arrowIcon}
+                      alt="arrowIcon"
+                      key={`${arrowIcon}_${item.name}`}
+                    />
+                  </button>
+                </td>
+                <td>
+                  <a target="_blank" href={item.preview} download>
+                    <img
+                      src={downloadIcon}
+                      alt="downloadIcon"
+                      key={`${downloadIcon}_${item.name}`}
+                    />
+                  </a>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
