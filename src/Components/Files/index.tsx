ae8c4669b798478b1609478598253bc9c1c2c50a
Maria <1709rt@gmail.ru>
2021-07-26 21:12:25 +0300

    File table refactoring

--- src/Components/Files/index.tsx
+++ src/Components/Files/index.tsx
@@ -5,7 +5,6 @@ import searchIcon from './img/searchIcon.svg';
 import arrow from './img/arrow.svg';
 import downloadIcon from './img/downloadIcon.svg';
 import { getTasks } from '../../services/tasks';
-import preview from '../TaskFiles/img/improvements.svg';
 
 interface Sort {
   sortBy: 'name' | 'size' | 'date' | 'uploadedBy';
@@ -20,7 +19,7 @@ const INITIAL_SORT: Sort = {
 function Files() {
   const [sort, setSort] = useState(INITIAL_SORT);
   const [files, setFiles] = useState<TaskFilesType[]>([]);
-  
+
   const getFiles = async () => {
     const allTasks = await getTasks();
     const allFiles: TaskFilesType[] = allTasks
@@ -78,126 +77,135 @@ function Files() {
 
   return (
     <div className='files'>
-      <table className='files__table'>
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>
-              <img src={searchIcon} alt='searchIcon' className='searchIcon' />
-              <button
-                onClick={() => {
-                  updateSortState({
-                    sortBy: 'name',
-                    sortType: sort.sortType === 'acs' ? 'desc' : 'acs',
-                  });
-                }}
-                className='sort-button'
-              >
-                Name
-                {sort.sortBy === 'name' && (
-                  <>
-                    {sort.sortType === 'acs' ? (
-                      <img alt='arrowUp' src={arrow} className='arrow up' />
-                    ) : (
-                      <img alt='arrowDown' src={arrow} className='arrow' />
-                    )}
-                  </>
-                )}
-              </button>
-            </th>
-            <th>
-              <button
-                onClick={() => {
-                  updateSortState({
-                    sortBy: 'size',
-                    sortType: sort.sortType === 'acs' ? 'desc' : 'acs',
-                  });
-                }}
-                className='sort-button'
-              >
-                Size
-                {sort.sortBy === 'size' && (
-                  <>
-                    {sort.sortType === 'acs' ? (
-                      <img alt='arrowUp' src={arrow} className='arrow up' />
-                    ) : (
-                      <img alt='arrowDown' src={arrow} className='arrow' />
-                    )}
-                  </>
-                )}
-              </button>
-            </th>
-            <th>
-              <button
-                onClick={() => {
-                  updateSortState({
-                    sortBy: 'uploadedBy',
-                    sortType: sort.sortType === 'acs' ? 'desc' : 'acs',
-                  });
-                }}
-                className='sort-button'
-              >
-                Uploaded By
-                {sort.sortBy === 'uploadedBy' && (
-                  <>
-                    {sort.sortType === 'acs' ? (
-                      <img alt='arrowUp' src={arrow} className='arrow up' />
-                    ) : (
-                      <img alt='arrowDown' src={arrow} className='arrow' />
-                    )}
-                  </>
-                )}
-              </button>
-            </th>
-            <th>Date</th>
-            <th>Tag</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredFiles.map((item) => {
-            return (
-              <tr key={`${item.id}_${item.name}`}>
-                <td>
-                  <img
-                    className='table__img'
-                    src={item.preview}
-                    alt={item.name}
-                    key={`${item.preview}_${item.name}`}
-                  />
-                </td>
-                <td>{item.name}</td>
-                <td>
-                  {item.size} {item.sizeSign}
-                </td>
-                <td>{item.uploadedBy}</td>
-                <td>{item.date}</td>
-                <td></td>
-                <td>
-                  <button className='table__button'>
-                    Actions{' '}
+      <div className='files__table'>
+        <div className='files__table-title'>
+          <div>Image</div>
+          <div>
+            <img src={searchIcon} alt='searchIcon' className='searchIcon' />
+            <button
+              onClick={() => {
+                updateSortState({
+                  sortBy: 'name',
+                  sortType: sort.sortType === 'acs' ? 'desc' : 'acs',
+                });
+              }}
+              className='button-sort'
+            >
+              Name
+              {sort.sortBy === 'name' && (
+                <>
+                  {sort.sortType === 'acs' ? (
                     <img
+                      alt='arrowUp'
                       src={arrow}
-                      alt='arrow'
-                      key={`${arrow}_${item.name}`}
+                      className='icon-arrow icon-arrow_up'
                     />
-                  </button>
-                </td>
-                <td>
-                  <a target='_blank' href={item.preview} download>
+                  ) : (
+                    <img alt='arrowDown' src={arrow} className='icon-arrow' />
+                  )}
+                </>
+              )}
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                updateSortState({
+                  sortBy: 'size',
+                  sortType: sort.sortType === 'acs' ? 'desc' : 'acs',
+                });
+              }}
+              className='button-sort'
+            >
+              Size
+              {sort.sortBy === 'size' && (
+                <>
+                  {sort.sortType === 'acs' ? (
                     <img
-                      src={downloadIcon}
-                      alt='downloadIcon'
-                      key={`${downloadIcon}_${item.name}`}
+                      alt='arrowUp'
+                      src={arrow}
+                      className='icon-arrow icon-arrow_up'
                     />
-                  </a>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                  ) : (
+                    <img alt='arrowDown' src={arrow} className='icon-arrow' />
+                  )}
+                </>
+              )}
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                updateSortState({
+                  sortBy: 'uploadedBy',
+                  sortType: sort.sortType === 'acs' ? 'desc' : 'acs',
+                });
+              }}
+              className='button-sort'
+            >
+              Uploaded By
+              {sort.sortBy === 'uploadedBy' && (
+                <>
+                  {sort.sortType === 'acs' ? (
+                    <img
+                      alt='arrowUp'
+                      src={arrow}
+                      className='icon-arrow icon-arrow_up'
+                    />
+                  ) : (
+                    <img alt='arrowDown' src={arrow} className='icon-arrow' />
+                  )}
+                </>
+              )}
+            </button>
+          </div>
+          <div>Date</div>
+          <div>Tag</div>
+          <div></div>
+          <div></div>
+        </div>
+        {filteredFiles.map((item) => {
+          return (
+            <div className='files__table-body' key={`${item.id}_${item.name}`}>
+              <div>
+                <img
+                  className='file-preview'
+                  src={item.preview}
+                  alt={item.name}
+                  key={`${item.preview}_${item.name}`}
+                />
+              </div>
+              <div>{item.name}</div>
+              <div>
+                {item.size} {item.sizeSign}
+              </div>
+              <div>{item.uploadedBy}</div>
+              <div>{item.date}</div>
+              <div></div>
+              <div>
+                <button className='button-actions'>
+                  Actions
+                  <img
+                    src={arrow}
+                    alt='arrow'
+                    key={`${arrow}_${item.name}`}
+                    className='icon-arrow'
+                  />
+                </button>
+              </div>
+              <div>
+                <a target='_blank' href={item.preview} download>
+                  <img
+                    src={downloadIcon}
+                    alt='downloadIcon'
+                    key={`${downloadIcon}_${item.name}`}
+                  />
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
