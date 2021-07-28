bb6c5d54e71b9cc17167ef35fee6655fbec57a3c
Maria <1709rt@gmail.ru>
2021-07-28 11:51:16 +0300

    added function to add  files in firestore

--- src/Components/Task/index.tsx
+++ src/Components/Task/index.tsx
@@ -6,11 +6,13 @@ import { TaskInfoBlock } from "../TaskInfoBlock";
 import { TaskDescription } from "../TaskDescription";
 import { TaskDiscussion } from "../TaskDiscussion";
 import { TaskFiles } from "../TaskFiles";
+import { TaskFilesType } from "../TaskFiles/types";
 
 import { user } from "../Sidebar";
 import { TaskType } from "./types";
 import { CommentType } from "../Comment/types";
 import firebase from "../../services/firebase";
+import headerPhoto from "../TaskFiles/img/headerPhoto.svg";
 
 export interface TaskProps {
   task: TaskType;
@@ -36,6 +38,48 @@ function Task({ task, onTaskChanged }: TaskProps) {
     onTaskChanged(newTask);
   };
 
+  //обновили все файлы в Task
+  const updateFiles = async (
+    newFile: TaskFilesType,
+    allFiles: TaskFilesType[]
+  ): Promise<void> => {
+    const newFiles = [newFile, ...allFiles];
+    const newTask: TaskType = {
+      ...task,
+      files: newFiles,
+    };
+    const db = firebase.firestore();
+
+    await db.collection("tasks").doc(task.id.toString()).set(newTask);
+
+    onTaskChanged(newTask); 
+  };
+
+
+
+  const createFile = (event: any):void => {
+    const file = event.target.files[0];
+    let preview = ''
+
+    const newFile: TaskFilesType = {
+      //генерировать в firestore id
+      id: Date.now().toString(),
+      preview: preview,
+      name: file.name,
+      size: file.size,
+      sizeSign: "KB",
+      uploadedBy: "",
+      date: Date.now().toString(),
+    };
+
+    const reader = new FileReader();
+    reader.onload = (event: any) => {
+      newFile.preview  = event.target.result;
+    };
+    reader.readAsDataURL(file); 
+    updateFiles(newFile, task.files);
+  };
+
   return (
     <div className="task">
       <TaskHeader
@@ -53,7 +97,7 @@ function Task({ task, onTaskChanged }: TaskProps) {
         <TaskInfoBlock title={"Followers"} users={task.followers} />
       </div>
       <TaskDescription text={task.description} />
-
+      <input type="file" onChange={createFile} />
       {task.files && (
         <div className="task__files">
           {task.files.map((item) => {
