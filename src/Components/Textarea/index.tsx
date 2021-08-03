318feeb8f724451ead9befa722b4e01623dfd71d
Maria <1709rt@gmail.ru>
2021-08-03 12:32:07 +0300

    Edited style

--- src/Components/Textarea/index.tsx
+++ src/Components/Textarea/index.tsx
@@ -1,26 +1,25 @@
-import React  from "react";
-import './style.scss'
+import React from "react";
+import "./style.scss";
 
 interface TextareaProps {
-    label: string,
-    handleDescription: (value: string) => void,
+  label: string;
+  handleDescription: (value: string) => void;
 }
 
-const Textarea = ({label, handleDescription}: TextareaProps) => {
-    return (
-        <div className="Textarea">
-        <div className="Textarea__label">{label}</div>
-        <div>
-          <textarea
-            className="Textarea__value"
-            onChange={(event) => {
-              handleDescription(event.target.value);
-            }}
-          ></textarea>
-        </div>
+const Textarea = ({ label, handleDescription }: TextareaProps) => {
+  return (
+    <div className="Textarea">
+      <div className="Textarea__label">{label}</div>
+      <div>
+        <textarea
+          className="Textarea__value"
+          onChange={(event) => {
+            handleDescription(event.target.value);
+          }}
+        ></textarea>
       </div>
-    )
+    </div>
+  );
+};
 
-}
-
-export {Textarea}
+export { Textarea };
