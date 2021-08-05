e4fa8539c486303f8e959997d118fe5924bf71e4
Maria <1709rt@gmail.ru>
2021-08-05 18:15:01 +0300

    Sidebar

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,100 +1,100 @@
-import React from 'react';
-import './style.css';
-import { UserType } from '../UserInfo/types';
-import { Brand } from '../Brand';
-import { UserInfo } from '../UserInfo';
-import userpic from '../UserInfo/img/userpic.png';
-import { Stats, StatsType } from '../Stats';
-import { SidebarMenu, ContentType } from '../SidebarMenu';
-import dashboard from '../SidebarMenu/img/dashboard.svg';
-import crm from '../SidebarMenu/img/crm.svg';
-import website from '../SidebarMenu/img/website.svg';
-import communication from '../SidebarMenu/img/communication.svg';
-import userZ from '../SidebarMenu/img/userZ.png';
-import userO from '../SidebarMenu/img/userO.png';
-import userT from '../SidebarMenu/img/userT.png';
-import userF from '../SidebarMenu/img/userF.png';
-import userFo from '../SidebarMenu/img/userFo.png';
-import userFa from '../SidebarMenu/img/userFa.png';
-import userSi from '../SidebarMenu/img/userSi.png';
-import userSe from '../SidebarMenu/img/userSe.png';
-import userEt from '../SidebarMenu/img/userEt.png';
-import { SidebarFooter } from '../SidebarFooter';
+import React from "react";
+import "./style.scss";
+import { UserType } from "../UserInfo/types";
+import { Brand } from "../Brand";
+import { UserInfo } from "../UserInfo";
+import userpic from "../UserInfo/img/userpic.png";
+import { Stats, StatsType } from "../Stats";
+import { SidebarMenu, ContentType } from "../SidebarMenu";
+import dashboard from "../SidebarMenu/img/dashboard.svg";
+import crm from "../SidebarMenu/img/crm.svg";
+import website from "../SidebarMenu/img/website.svg";
+import communication from "../SidebarMenu/img/communication.svg";
+import userZ from "../SidebarMenu/img/userZ.png";
+import userO from "../SidebarMenu/img/userO.png";
+import userT from "../SidebarMenu/img/userT.png";
+import userF from "../SidebarMenu/img/userF.png";
+import userFo from "../SidebarMenu/img/userFo.png";
+import userFa from "../SidebarMenu/img/userFa.png";
+import userSi from "../SidebarMenu/img/userSi.png";
+import userSe from "../SidebarMenu/img/userSe.png";
+import userEt from "../SidebarMenu/img/userEt.png";
+import { SidebarFooter } from "../SidebarFooter";
 
 export const user: UserType = {
   avatar: userpic,
-  initials: 'Emilee Simchenko',
-  position: 'Product Owner',
+  initials: "Emilee Simchenko",
+  position: "Product Owner",
 };
 
 const tasks: StatsType[] = [
-  { name: 'Completed Tasks', counter: 372 },
-  { name: 'Open Tasks', counter: 11 },
+  { name: "Completed Tasks", counter: 372 },
+  { name: "Open Tasks", counter: 11 },
 ];
 
 const menu: ContentType[] = [
-  { name: 'Home' },
-  { name: 'My Tasks' },
-  { name: 'Notifications', counter: 3 },
+  { name: "Home" },
+  { name: "My Tasks" },
+  { name: "Notifications", counter: 3 },
 ];
 
 const projects: ContentType[] = [
   {
-    name: 'Dashboard UI Kit',
+    name: "Dashboard UI Kit",
     preview: dashboard,
   },
   {
-    name: 'CRM System',
+    name: "CRM System",
     preview: crm,
   },
   {
-    name: 'Website Redesign',
+    name: "Website Redesign",
     preview: website,
   },
   {
-    name: 'Communication Tool',
+    name: "Communication Tool",
     preview: communication,
   },
 ];
 
 const teams: ContentType[] = [
   {
-    name: 'Designers',
+    name: "Designers",
     items: [
-      { name: 'userZ', avatar: userZ },
-      { name: 'userO', avatar: userO },
-      { name: 'userT', avatar: userT },
+      { name: "userZ", avatar: userZ },
+      { name: "userO", avatar: userO },
+      { name: "userT", avatar: userT },
     ],
   },
   {
-    name: 'Backend',
+    name: "Backend",
     items: [
-      { name: 'userF', avatar: userF },
-      { name: 'userFo', avatar: userFo },
+      { name: "userF", avatar: userF },
+      { name: "userFo", avatar: userFo },
     ],
   },
   {
-    name: 'Frontend',
+    name: "Frontend",
     items: [
-      { name: 'userFa', avatar: userFa },
-      { name: 'userSi', avatar: userSi },
-      { name: 'userSe', avatar: userSe },
-      { name: 'userEt', avatar: userEt },
+      { name: "userFa", avatar: userFa },
+      { name: "userSi", avatar: userSi },
+      { name: "userSe", avatar: userSe },
+      { name: "userEt", avatar: userEt },
     ],
   },
 ];
 
 function Sidebar() {
   return (
-    <div className='sidebar'>
+    <div className="Sidebar">
       <Brand />
       <UserInfo user={user} />
       <Stats tasks={tasks} />
-      <SidebarMenu titel={'menu'} content={menu} />
-      <SidebarMenu titel={'projects'} content={projects} />
-      <button className='sidebar__button'>+ Add a Project</button>
-      <SidebarMenu titel={'teams'} content={teams} />
-      <button className='sidebar__button'>+ Add a Team</button>
+      <SidebarMenu titel={"menu"} content={menu} />
+      <SidebarMenu titel={"projects"} content={projects} />
+      <button className="Sidebar__button">+ Add a Project</button>
+      <SidebarMenu titel={"teams"} content={teams} />
+      <button className="Sidebar__button">+ Add a Team</button>
       <SidebarFooter />
     </div>
   );
