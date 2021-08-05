8de481949b09a62c4059810f18d4968da9bc14c8
Maria <1709rt@gmail.ru>
2021-08-05 18:11:14 +0300

    Reorganized component HeaderTabs

--- src/Components/HeaderTabs/index.tsx
+++ src/Components/HeaderTabs/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './style.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./style.scss";
 
 export type TabsType = {
   name: string;
@@ -13,14 +13,14 @@ interface TabsProps {
 
 function HeaderTabs({ items }: TabsProps) {
   return (
-    <div className='tabs'>
+    <div className="Tabs">
       {items.map((item) => {
         return (
           item?.name && (
             <NavLink
               to={item.path}
-              className='tabs__item'
-              activeClassName='tabs__item_active'
+              className="Tabs__item"
+              activeClassName="Tabs__item_active"
               key={`${item.name}`}
             >
               {item.name}
