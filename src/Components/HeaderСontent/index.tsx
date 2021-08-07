a02b836efd46f98e7bdfc7dd74e221c15a27290f
Maria <1709rt@gmail.ru>
2021-08-07 11:06:29 +0300

    refactoring

--- src/Components/HeaderСontent/index.tsx
+++ src/Components/HeaderСontent/index.tsx
@@ -5,14 +5,7 @@ import { Members } from "../Members";
 import userN from "../Members/img/userN.png";
 import userTen from "../Members/img/userTen.png";
 import userEl from "../Members/img/userEl.png";
-import { user } from "../Sidebar";
 import { UserType } from "../UserInfo/types";
-
-// const members: MembersType[] = [
-//   { name: 'userN', avatar: userN, position: "Product Owner"},
-//   { name: 'userTen', avatar: userTen, position: "Product Owner"},
-//   { name: 'userEl', avatar: userEl, position: "Product Owner" },
-// ];
 
 const members: UserType[] = [
   { initials: "userN", avatar: userN, position: "Product Owner" },
