3daffd1cd9abe0734dcdb22b065ff5f0a8fcfa92
Maria <1709rt@gmail.ru>
2021-09-16 18:32:26 +0300

    Refactored, Complemented Icons + TaskFiles

--- src/Components/Icons/index.tsx
+++ src/Components/Icons/index.tsx
@@ -8,15 +8,32 @@ import circle from "./img/circle.svg";
 import chat from "./img/chat.svg";
 import add from "./img/add.svg";
 import download from "./img/download.svg";
+import downloadCloud from "./img/downloadCloud.svg";
+import deleteIcon from "./img/delete.svg";
+
+export enum ICON_SIZE {
+  SMALL = "SMALL",
+  MEDIUM = "MEDIUM",
+  LARGE = "LARGE",
+}
 
 interface IconsProps {
+  size?: ICON_SIZE;
   name: string;
   url?: string;
 }
 
-const Icons = ({ name, url }: IconsProps): JSX.Element => {
+const Icons = ({ name, url, size }: IconsProps): JSX.Element => {
   let className = "Icon ";
   let scr;
+
+  if (size === ICON_SIZE.SMALL) {
+    className += "Icon_small ";
+  } else if (size === ICON_SIZE.MEDIUM) {
+    className += "Icon_medium ";
+  } else if (size === ICON_SIZE.LARGE) {
+    className += "Icon_large ";
+  }
 
   switch (true) {
     case name == "list":
@@ -41,7 +58,6 @@ const Icons = ({ name, url }: IconsProps): JSX.Element => {
 
     case name == "previewProject":
       scr = url;
-      className = "Icon__preview-project ";
       break;
 
     case name == "chat":
@@ -54,6 +70,14 @@ const Icons = ({ name, url }: IconsProps): JSX.Element => {
 
     case name == "download":
       scr = download;
+      break;
+
+    case name == "delete":
+      scr = deleteIcon;
+      break;
+
+    case name == "downloadCloud":
+      scr = downloadCloud;
       break;
   }
   return <img src={scr} alt={name} className={className}></img>;
