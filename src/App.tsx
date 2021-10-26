4d3ca63678222ce494212083e5abef556dd6818d
Maria <1709rt@gmail.ru>
2021-10-26 22:17:04 +0300

    created AvailableSoon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
-import './reset.css';
-import './App.css';
-import { Sidebar } from './Components/Sidebar';
-import { Header } from './Components/Header';
-import { Tasks } from './Components/Tasks';
-import { Files } from './Components/Files';
+import React from "react";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import "./reset.css";
+import "./App.css";
+import { Sidebar } from "./Components/Sidebar";
+import { Header } from "./Components/Header";
+import { Tasks } from "./Components/Tasks";
+import { Files } from "./Components/Files";
+import { AvailableSoon } from "./Components/AvailableSoon";
 
 function App() {
   return (
     <HashRouter>
-      <div className='wrapper'>
+      <div className="wrapper">
         <Sidebar />
         <div>
           <Header />
           <Switch>
-            <Redirect exact from='/' to='/tasks' />
-            <Route path='/tasks'>
+            <Redirect exact from="/" to="/tasks" />
+            <Route path="/tasks">
               <Tasks />
             </Route>
-            <Route path='/files'>
+            <Route path="/files">
               <Files />
+            </Route>
+            <Route path="/kanban">
+              <AvailableSoon />
+            </Route>
+
+            <Route path="/activity">
+              <AvailableSoon />
+            </Route>
+
+            <Route path="/calendar">
+              <AvailableSoon />
             </Route>
           </Switch>
         </div>
