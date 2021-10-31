349a1f704165e9bef31b39c3f00b2aec774fb386
Maria Arsenkova <1709rt@gmail.com>
2021-10-31 21:18:50 +0300

    Update index.tsx

--- src/Components/TaskFiles/index.tsx
+++ src/Components/TaskFiles/index.tsx
@@ -60,35 +60,6 @@ function TaskFiles({ file, onRemoveFile }: TaskFilesProps) {
             <div className="TaskFiles__icon-control">
               <Icon name={"delete"} size={ICON_SIZE.SMALL}  transparency={ICON_TRANSPARENCY.HALF}/>
             </div>
-            {/* <div className="test">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                version="1.1"
-                viewBox="0 0 32 32"
-              >
-                <g transform="scale(2)">
-                  <circle fill="#ffffff" id="facebook" cx="8" cy="8" r="7" />
-                  <rect
-                    fill="#f44336"
-                    width="2"
-                    height="10"
-                    x="-.98"
-                    y="-16.29"
-                    transform="rotate(135)"
-                  />
-                  <rect
-                    fill="#f44336"
-                    width="2"
-                    height="10"
-                    x="-12.29"
-                    y="-5.01"
-                    transform="rotate(-135)"
-                  />
-                </g>
-              </svg>
-            </div> */}
           </button>
         </div>
       </div>
