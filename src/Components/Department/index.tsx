a68e6677283c5039201e9159eced2b4072623fb6
Maria <maria.arsenkova@gmail.com>
2021-11-09 21:01:02 +0300

    fix from == to ===

--- src/Components/Department/index.tsx
+++ src/Components/Department/index.tsx
@@ -14,11 +14,11 @@ export type DepartmentType = {
 const Department = ({ department }: DepartmentType) => {
   let className = "Department ";
 
-  if (department == DEPARTMENT.DESIGN) {
+  if (department === DEPARTMENT.DESIGN) {
     className += "Department_design";
-  } else if (department == DEPARTMENT.DEVELOPEMENT) {
+  } else if (department === DEPARTMENT.DEVELOPEMENT) {
     className += "Department_developement";
-  } else if (department == DEPARTMENT.MARKETING) {
+  } else if (department === DEPARTMENT.MARKETING) {
     className += "Department_marketing";
   }
   return <span className={className}>{department}</span>;
